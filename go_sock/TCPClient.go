30b03204c602f3823b3bc44083f0272d312e60a7
Junjie <liujary@gmail.com>
2016-09-30 00:36:29 -0500

    update

--- go_sock/TCPClient.go
+++ go_sock/TCPClient.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "net"
-  "os"
-  "time"
-  "bufio"
-  "sync"
+	"bufio"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 //import "strconv"
@@ -26,82 +27,88 @@ var roundFile string
 
 /* A Simple function to verify error */
 func CheckErrorExit(errString string, err error) {
-  if err != nil {
-    fmt.Println(errString+": ", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(errString+": ", err)
+		os.Exit(1)
+	}
 }
 
 func handleTCP(wg *sync.WaitGroup, id int, recordOrNot bool) {
-  var conn net.Conn
-  var err error
-  conn, err = net.Dial(ctype, ip+":"+port)
-  CheckErrorExit("TCP Connection Error", err)
-
-  buf := make([]byte, msglen)
+	var conn net.Conn
+	var err error
+	conn, err = net.Dial(ctype, ip+":"+port)
+	CheckErrorExit("TCP Connection Error", err)
+	gap := stopNum / 10000
+	buf := make([]byte, msglen)
+	oneWayLatencies := make([]int, 10000)
 	//time.Sleep(time.Microsecond * time.Duration(interval*rand.Intn(1000)))
-  startTime := time.Now().UnixNano()
-  i := 0
-	for i=0; i < stopNum; i++ {
+	startTime := time.Now().UnixNano()
+	i := 0
+	for i = 0; i < stopNum; i++ {
 		n, err := conn.Read(buf)
 		if err != nil {
-      fmt.Println("Read Error:", err)
-      conn.Close()
-      break
-    }
-    for n < msglen && n != 0 {
-      m, err := conn.Read(buf[n:])
-      if err != nil {
-        fmt.Println("Read Error:", err)
-        conn.Close()
-        break
-      }
-      n += m
-    }
-    if n == 0 {
-      fmt.Println("recieve 0")
-    }
+			fmt.Println("Read Error:", err)
+			conn.Close()
+			break
+		}
+		for n < msglen && n != 0 {
+			m, err := conn.Read(buf[n:])
+			if err != nil {
+				fmt.Println("Read Error:", err)
+				conn.Close()
+				break
+			}
+			n += m
+		}
+		currentTime := time.Now().UnixNano()
+		if n == 0 {
+			fmt.Println("recieve 0")
+		}
+		sentNum, _ := binary.Varint(bufferRcv)
+		serverSentTime, _ := binary.Varint(bufferRcv[8:])
+		if rcvPkt%gap == 0 {
+			oneWayLatencies[rcvPkt] = currentTime - serverSentTime
+		}
 	}
-  endTime := time.Now().UnixNano()
-  fmt.Println(endTime-startTime, " ns passed")
-  fmt.Println(i, "packets received")
+	endTime := time.Now().UnixNano()
+	fmt.Println(endTime-startTime, " ns passed")
+	fmt.Println(i, "packets received")
 	wg.Done()
 }
 
 func writeLines(lines []int, path string) error {
-  file, err := os.Create(path)
-  if err != nil {
-    return err
-  }
-  defer file.Close()
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-  w := bufio.NewWriter(file)
-  for _, line := range lines {
-    fmt.Fprintln(w, line)
-  }
-  return w.Flush()
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
 
 func main() {
 
+	flag.IntVar(&interval, "i", 5, "The interval of sending message in ms")
+	flag.IntVar(&msglen, "l", 1000, "The message length")
+	flag.IntVar(&cnum, "n", 1, "Number of concurrent client connections")
+	flag.StringVar(&ip, "a", "127.0.0.1", "The IP address of remote server")
+	flag.StringVar(&port, "p", "8080", "The port number of remote server")
+	flag.StringVar(&ctype, "t", "tcp", "The connection type, TCP or UDP or UDT")
+	flag.IntVar(&stopNum, "s", 400, "Number of message to send before stop")
+	flag.IntVar(&recordlen, "rl", 200, "The number of latency to record")
+	flag.StringVar(&oneFile, "of", "client_oneWay", "The file name for one way latency")
+	flag.StringVar(&roundFile, "rf", "client_roundtrip", "The file name for round trip latancy")
+	flag.Parse()
 
-  flag.IntVar(&interval, "i", 5, "The interval of sending message in ms")
-  flag.IntVar(&msglen, "l", 1000, "The message length")
-  flag.IntVar(&cnum, "n", 1, "Number of concurrent client connections")
-  flag.StringVar(&ip, "a", "127.0.0.1", "The IP address of remote server")
-  flag.StringVar(&port, "p", "8080", "The port number of remote server")
-  flag.StringVar(&ctype, "t", "tcp", "The connection type, TCP or UDP or UDT")
-  flag.IntVar(&stopNum, "s", 400, "Number of message to send before stop")
-  flag.IntVar(&recordlen, "rl", 200, "The number of latency to record")
-  flag.StringVar(&oneFile, "of", "client_oneWay", "The file name for one way latency")
-  flag.StringVar(&roundFile, "rf", "client_roundtrip", "The file name for round trip latancy")
-  flag.Parse()
-
-  var wg sync.WaitGroup
-  for i:= 1; i <= cnum; i++ {
-      wg.Add(1)
-      go handleTCP(&wg, i, false)
-  }
-  wg.Wait()
-  //fmt.Println("END Client Program")
+	var wg sync.WaitGroup
+	for i := 1; i <= cnum; i++ {
+		wg.Add(1)
+		go handleTCP(&wg, i, false)
+	}
+	wg.Wait()
+	//fmt.Println("END Client Program")
 }
--- go_sock/UDPClient.go
+++ go_sock/UDPClient.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "flag"
-  "net"
-  "fmt"
-  "os"
-  "time"
-  "bufio"
-  "encoding/binary"
-  "math/rand"
-  "sync"
+	"bufio"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 const (
-  BUFSIZE = 2048
+	BUFSIZE = 2048
 )
 
 //import "strconv"
@@ -33,112 +33,118 @@ var pktLost int
 
 /* A Simple function to verify error */
 func CheckErrorExit(errString string, err error) {
-  if err != nil {
-    fmt.Println(errString+": ", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(errString+": ", err)
+		os.Exit(1)
+	}
 }
 
+oneWayLatencies := make([]int, 10000)
+
 func handleUDP(wg *sync.WaitGroup, id int, recordOrNot bool) {
-  //time.Sleep(time.Microsecond * time.Duration(interval*rand.Intn(1000)))
-  var err error
-    /* Lets prepare a address at any address at port 10001*/
-  localAddr, err := net.ResolveUDPAddr("udp", ":0")
-  CheckErrorExit("Resolve UDP ERROR", err)
+	//time.Sleep(time.Microsecond * time.Duration(interval*rand.Intn(1000)))
+	var err error
+	/* Lets prepare a address at any address at port 10001*/
+	localAddr, err := net.ResolveUDPAddr("udp", ":0")
+	CheckErrorExit("Resolve UDP ERROR", err)
 
-  serverAddr, err := net.ResolveUDPAddr("udp", ip+":"+port)
-  CheckErrorExit("Resolve UDP ERROR", err)
+	serverAddr, err := net.ResolveUDPAddr("udp", ip+":"+port)
+	CheckErrorExit("Resolve UDP ERROR", err)
 
-  conn, err := net.DialUDP("udp", localAddr, serverAddr)
-  CheckErrorExit("UDP Dial Error", err)
+	conn, err := net.DialUDP("udp", localAddr, serverAddr)
+	CheckErrorExit("UDP Dial Error", err)
 
-  //buffer := make([]byte, msglen)
-  bufferRcv := make([]byte, msglen)
-  buff := make([]byte, msglen)
-  binary.PutVarint(buff, 0)
-  //binary.PutVarint(buff[8:], )
-  conn.Write(buff)
-  lostPacket := make([]int, 20000)
-  startTime := time.Now().UnixNano()
-  lostPkt := 0
-  rcvPkt := 0
-  i := 0
-  oneWayLatencies = make([]int, 10000)
+	//buffer := make([]byte, msglen)
+	bufferRcv := make([]byte, msglen)
+	buff := make([]byte, msglen)
+	binary.PutVarint(buff, 0)
+	//binary.PutVarint(buff[8:], )
+	conn.Write(buff)
+	lostPacket := make([]int, 20000)
+	startTime := time.Now().UnixNano()
+	lostPkt := 0
+	rcvPkt := 0
+	i := 0
 
-  conn.SetReadBuffer(10*1024*1024) // setup the read buffer as 10MB.
-	for i=0; i < stopNum; i++ {
-    //conn.SetReadDeadline(time.Now().Add(time.Second*1))
-    n,err := conn.Read(bufferRcv)
-    currentTime := time.Now().UnixNano()
-    if n!=msglen {
-      fmt.Println("expecting ", msglen, " Bytes and recieved ", n, " Bytes")
+	conn.SetReadBuffer(4 * 1024 * 1024) // setup the read buffer as 10MB.
+	conn.SetWriteBuffer(4 * 1024 * 1024)
+  gap := stopNum/10000
+	for i = 0; i < stopNum; i++ {
+		//conn.SetReadDeadline(time.Now().Add(time.Second*1))
+		n, err := conn.Read(bufferRcv)
+		currentTime := time.Now().UnixNano()
+		if n != msglen {
+			fmt.Println("expecting ", msglen, " Bytes and recieved ", n, " Bytes")
+		}
+		if err != nil {
+			fmt.Println("Read Error:", err)
+			conn.Close()
+			break
+		}
+		sentNum, _ := binary.Varint(bufferRcv)
+		serverSentTime, _ := binary.Varint(bufferRcv[8:])
+    if rcvPkt % gap == 0 {
+		  oneWayLatencies[rcvPkt] = currentTime - serverSentTime
     }
-    if err != nil  {
-      fmt.Println("Read Error:", err)
-      conn.Close()
-      break
-    }
-    sentNum, _ := binary.Varint(bufferRcv)
-    serverSentTime, _ := binary.Varint(bufferRcv[8:])
-    if int(sentNum) > i && lostPkt+2 < 20000{
-      lostPacket[lostPkt] = i
-      lostPacket[lostPkt+1] = int(sentNum)-1
-      lostPkt += 2
-      i = int(sentNum)
-    }
-    rcvPkt ++
-  }
-  if lostPkt+1 < 20000{
-    lostPacket[lostPkt] = i
-    lostPkt ++
-  }
-  endTime := time.Now().UnixNano()
-  fmt.Println(endTime-startTime, " ns passed")
-  fmt.Println(rcvPkt, "packets received")
-  writeLines(lostPacket, "lostpkt.log")
-  wg.Done()
+		// if int(sentNum) > i && lostPkt+2 < 20000{
+		//   lostPacket[lostPkt] = i
+		//   lostPacket[lostPkt+1] = int(sentNum)-1
+		//   lostPkt += 2
+		//   i = int(sentNum)
+		// }
+		rcvPkt++
+	}
+	// if lostPkt+1 < 20000 {
+	// 	lostPacket[lostPkt] = i
+	// 	lostPkt++
+	// }
+	endTime := time.Now().UnixNano()
+	fmt.Println(endTime-startTime, " ns passed")
+	fmt.Println(rcvPkt, "packets received")
+	//writeLines(lostPacket, "lostpkt.log")
+	writeLines(oneWayLatencies, "latency.log")
+	wg.Done()
 }
 
 func writeLines(lines []int, path string) error {
-  file, err := os.Create(path)
-  if err != nil {
-    return err
-  }
-  defer file.Close()
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-  w := bufio.NewWriter(file)
-  for _, line := range lines {
-    fmt.Fprintln(w, line)
-  }
-  return w.Flush()
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
 
 func main() {
 
+	flag.IntVar(&interval, "i", 5, "The interval of sending message in ms")
+	flag.IntVar(&msglen, "l", 1000, "The message length")
+	flag.IntVar(&cnum, "n", 1, "Number of concurrent client connections")
+	flag.StringVar(&ip, "a", "127.0.0.1", "The IP address of remote server")
+	flag.StringVar(&port, "p", "8080", "The port number of remote server")
+	flag.StringVar(&ctype, "t", "udp", "The connection type, TCP or UDP or UDT")
+	flag.IntVar(&stopNum, "s", 400, "Number of message to send before stop")
+	flag.IntVar(&recordlen, "rl", 200, "The number of latency to record")
+	flag.StringVar(&oneFile, "of", "udp_oneway", "The file name for one way latency")
+	flag.StringVar(&roundFile, "rf", "udp_roundtrip", "The file name for round trip latancy")
+	flag.Parse()
 
-  flag.IntVar(&interval, "i", 5, "The interval of sending message in ms")
-  flag.IntVar(&msglen, "l", 1000, "The message length")
-  flag.IntVar(&cnum, "n", 1, "Number of concurrent client connections")
-  flag.StringVar(&ip, "a", "127.0.0.1", "The IP address of remote server")
-  flag.StringVar(&port, "p", "8080", "The port number of remote server")
-  flag.StringVar(&ctype, "t", "udp", "The connection type, TCP or UDP or UDT")
-  flag.IntVar(&stopNum, "s", 400, "Number of message to send before stop")
-  flag.IntVar(&recordlen, "rl", 200, "The number of latency to record")
-  flag.StringVar(&oneFile, "of", "udp_oneway", "The file name for one way latency")
-  flag.StringVar(&roundFile, "rf", "udp_roundtrip", "The file name for round trip latancy")
-  flag.Parse()
-
-  var wg sync.WaitGroup
-  var recordOrNot bool
-  recordNum := rand.Intn(cnum)
-  for i := 1; i <= cnum; i++ {
-    recordOrNot = false
-    if i-1 == recordNum {
-      recordOrNot = true
-    }
-    wg.Add(1)
-    go handleUDP(&wg, i, recordOrNot)
-  }
-  wg.Wait()
-  fmt.Println("END Client Program, ", pktLost, " packets lost")
+	var wg sync.WaitGroup
+	var recordOrNot bool
+	recordNum := rand.Intn(cnum)
+	for i := 1; i <= cnum; i++ {
+		recordOrNot = false
+		if i-1 == recordNum {
+			recordOrNot = true
+		}
+		wg.Add(1)
+		go handleUDP(&wg, i, recordOrNot)
+	}
+	wg.Wait()
+	fmt.Println("END Client Program, ", pktLost, " packets lost")
 }
