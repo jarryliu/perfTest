3f88f685c7e29886d04fe5adb23c177a1c740ed9
Junjie <liujary@gmail.com>
2016-10-06 15:20:20 -0500

    update

--- c_sock/udpclient.c
+++ c_sock/udpclient.c
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
     int portno, n;
     struct sockaddr_in serveraddr, clientaddr;
     struct hostent *server;
-    int slen = size(serveraddr)
+    int slen = size(serveraddr);
     char *hostname;
     char buf[BUFSIZE];
 
